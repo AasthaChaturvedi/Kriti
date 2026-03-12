27e648800941b281b462572c2b7b7cb5bf8e079e
aastha-git9 <aasthachaturvedi0908@gmail.com>
2026-03-12 16:08:20 +0530

    Add Study Planner Task input and task list

--- src/pages/Planner.jsx
+++ src/pages/Planner.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
 function Planner () {
+    const [subject, setSubject] =useState ("");
+    const [tasks, settasks] =useState ([]);
+
+    const addTask = () => {
+        settasks([...tasks, subject]);
+        setSubject("");
+    };
+
+
     return (
         <div>
-            <h1>Planner Page</h1>
+            <h1>Study Planner</h1>
+            <input
+                type="text"
+                value={subject}
+                onChange={(e) => setSubject(e.target.value)}
+                placeholder="Enter subject"
+            />
+            <button onClick={addTask}>Add Task</button>
+            <ul>
+                {tasks.map((task, index) => (
+                    <li key={index}>{task}</li>
+                ))}
+            </ul>
         </div>
     );
 }
