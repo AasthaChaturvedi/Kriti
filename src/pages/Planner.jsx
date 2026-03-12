4b7f254e6bda5c03bab07670c89204b4df7b7532
aastha-git9 <aasthachaturvedi0908@gmail.com>
2026-03-12 19:10:16 +0530

    Add localStorage support to persist tasks

--- src/pages/Planner.jsx
+++ src/pages/Planner.jsx
@@ -1,30 +1,109 @@
-import { useState } from "react";
-function Planner () {
-    const [subject, setSubject] =useState ("");
-    const [tasks, settasks] =useState ([]);
+import { useState, useEffect, use } from "react";
 
-    const addTask = () => {
-        settasks([...tasks, subject]);
-        setSubject("");
+function Planner() {
+
+  const [subject, setSubject] = useState("");
+  const [duration, setDuration] = useState("");
+  const [priority, setPriority] = useState("");
+  const [notes, setNotes] = useState("");
+  const [date, setDate] = useState("");
+
+  const [tasks, setTasks] = useState([]);
+
+ 
+ useEffect(() => {
+  const savedTasks = localStorage.getItem("tasks");
+
+  if (savedTasks) {
+    setTasks(JSON.parse(savedTasks));
+  }
+}, []);
+
+useEffect(() => {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}, [tasks]);
+
+  
+
+  const addTask = () => {
+
+    const newTask = {
+      subject,
+      duration,
+      priority,
+      notes,
+      date,
+     
     };
 
+    setTasks([...tasks, newTask]);
 
-    return (
-        <div>
-            <h1>Study Planner</h1>
-            <input
-                type="text"
-                value={subject}
-                onChange={(e) => setSubject(e.target.value)}
-                placeholder="Enter subject"
-            />
-            <button onClick={addTask}>Add Task</button>
-            <ul>
-                {tasks.map((task, index) => (
-                    <li key={index}>{task}</li>
-                ))}
-            </ul>
-        </div>
-    );
+    setSubject("");
+    setDuration("");
+    setPriority("");
+    setNotes("");
+    setDate("");
+    
+  };
+
+  const deleteTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div>
+
+      <h1>Study Planner</h1>
+
+      <input
+        type="text"
+        placeholder="Subject"
+        value={subject}
+        onChange={(e) => setSubject(e.target.value)}
+      />
+
+      <input
+        type="text"
+        placeholder="Duration"
+        value={duration}
+        onChange={(e) => setDuration(e.target.value)}
+      />
+
+      <input
+        type="text"
+        placeholder="Priority"
+        value={priority}
+        onChange={(e) => setPriority(e.target.value)}
+      />
+
+      
+      <input
+        type="text"
+        placeholder="Notes"
+        value={notes}
+        onChange={(e) => setNotes(e.target.value)}
+      />
+
+      <input
+        type="date"
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+      />
+     
+      <button onClick={addTask}>Add Task</button>
+
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task.subject} - {task.duration} - {task.priority} - {task.deadline} - {task.notes} - {task.date}
+            <button onClick={() => deleteTask(index)}>❌</button>
+          </li>
+        ))}
+      </ul>
+
+    </div>
+  );
 }
-export default  Planner;
+
+export default Planner;
